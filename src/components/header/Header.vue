a912d7b907378e1101f294c5b91e3b067a0bdee7
limingru <18813070947@163.com>
2017-08-06 09:49:27 +0800

    header完成一半，准备开发弹层

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -12,12 +12,20 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div class="support" v-if="seller.supports">
-          <span class="icon"></span>
-          <span class="text">{{seller.supports[0].description}}</span>
+          <span class="icon" v-bind:class="classNames[seller.supports[0].type]"></span><span class="text">{{seller.supports[0].description}}</span>
         </div>
+      </div>
+      <div class="support-count" v-if="seller.supports">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
     <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span><i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img v-bind:src="seller.avatar" alt=""/>
     </div>
   </div>
 </template>
@@ -27,6 +35,7 @@
     props: ['seller'],
     data () {
       return {
+        classNames: ['decrease', 'discount', 'special', 'invoice', 'guarantee']
       }
     },
     created () {
@@ -39,9 +48,9 @@
   @import '../../common/scss/mixin.scss';
 
   .header{
-    background:black; color:#fff;
+    position:relative; background-color:rgba(7,17,27,0.5); color:#fff;
     .content-wrapper{
-      padding:24px 12px 18px 24px; font-size:0;
+      position:relative; padding:24px 12px 18px 24px; font-size:0;
       .avatar{
         display:inline-block; border-radius:2px; vertical-align:top;
         img{
@@ -53,7 +62,7 @@
         .title{
           margin:2px 0 8px 0;
           .brand{
-            display:inline-block; margin-right:6px; width:30px; height:18px; vertical-align:top;
+            display:inline-block; vertical-align:top; margin-right:6px; width:30px; height:18px;
             @include background-image(brand); background-size: 30px 18px; background-repeat:no-repeat;
           }
           .name{
@@ -63,9 +72,51 @@
         .description{
           margin-bottom:10px; font-size:12px; font-weight:200; color:rgb(255,255,255); line-heigth:12px;
         }
+        .support{
+          .icon{
+            display:inline-block; vertical-align:top; width:12px; height:12px; margin-right:4px; background-size:12px 12px;
+            background-repeat:no-repeat;
+            @include icon;
+          }
+          .text{
+            line-height:12px; font-size:10px;
+          }
+        }
 
       }
+      .support-count{
+        position:absolute; right: 12px; bottom:18px; padding:0 7px 0 9px; height:24px; background-color:rgba(0,0,0,0.2);
+        font-size:0px; line-height: 24px; color:rgb(255,255,255); border-radius:14px; text-align:center;
+        .count{
+          font-size:10px;
+        }
+        .icon-keyboard_arrow_right{
+          font-size:10px;
+        }
+      }
+
     }
+    .bulletin-wrapper{
+      position:relative; line-height:28px; height:28px; padding:0 22px 0 12px; background-color:rgba(7,17,27,0.2);
+      white-space:nowrap; overflow:hidden; text-overflow:ellipsis; font-size:10px; color:rgb(255,255,255); font-weight:200;
+      .bulletin-title{
+        display:inline-block; vertical-align:top; width:22px; height:12px; margin-top:7px;
+        @include background-image(bulletin); background-size:22px 12px; background-repeat:no-repeat;
+      }
+      .bulletin-text{
+        vertical-align:top; font-size:10px;
+      }
+      .icon-keyboard_arrow_right{
+        position:absolute; right:12px; bottom:10px; font-size:10px;
+      }
+    }
+    .background{
+      position:absolute; top:0; left:0; width:100%; height:100%; z-index:-1; filter:blur(5px); overflow:hidden;
+      img{
+        width:70%; height:100%; position:absolute; left:15%; top:20%;
+      }
+    }
+
   }
 
 </style>
